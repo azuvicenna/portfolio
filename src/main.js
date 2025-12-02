8fade86afc8e95b72421531d1f45e18b42b8b11b
Reeze <azfaalharits25@gmail.com>
2025-12-02 21:18:36 +0700

    feat: add internationalization support with vue-i18n; implement English and Indonesian translations

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from "pinia";
 // import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
+import i18n from './i18n'
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -12,4 +13,5 @@ const pinia = createPinia();
 // pinia.use(piniaPluginPersistedstate);
 app.use(router);
 app.use(pinia);
+app.use(i18n);
 app.mount("#app");
